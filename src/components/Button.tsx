282f620163beffe165205bb09e2e39d116aa19c7
Vítor Arimatéa <vitorodev@outlook.com>
2023-03-03 16:47:57 -0300

    Adicionando tipo any no styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Button = ({styles}) => (
+const Button = ({styles}:any) => (
     <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] rounded-[10px] text-primary outline-none ${styles}`}>
       Get Started
     </button>
