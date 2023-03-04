21bb525a4456231ea901522afd3366c82ec75f60
Vítor Arimatéa <vitorodev@outlook.com>
2023-03-03 22:33:24 -0300

    Melhorando legibilidade do código

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,58 +4,40 @@ import { close, logo, menu} from '../assets';
 import {navLinks} from '../constants';
 
 const Navbar = () => {
-  const [toggle, settoggle] = useState(false)
+  const [toggle, settoggle] = useState(false);
 
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
-        <img src={logo} alt="iMSafeBank" className="h-[32px] w-[124px]" />
+      <img src={logo} alt="iMSafeBank" className="h-[32px] w-[124px]" />
 
-          <ul className="list-none sm:flex hidden justify-end items-center flex-1 z-[2]">
-              {navLinks.map((nav: any, index: number) => (
-                  <li
-                      key={nav.id}
-                      className={`font-poppins font-normal cursor-pointer text[16px] ${
-                          index === navLinks.length - 1 ? "mr-0" : "mr-10"
-                      } text-white`}
-                  >
-                      <a href={`#${nav.id}`}>{nav.title}</a>
-                  </li>
-              ))}
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1 z-[2]">
+        {navLinks.map((nav: any, index: number) => (
+        
+          <li key={nav.id} className={`font-poppins font-normal cursor-pointer text[16px] ${index === navLinks.length - 1 ? "mr-0" : "mr-10"} text-white`}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img src={toggle ? close : menu} alt="menu" className="h-[28px] w-[28px] object-contain cursor-pointer" onClick={() => settoggle((prev) => !prev)}/>
+
+        <div className={`${toggle ? "flex" : "hidden"} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
+                  
+          <ul className="list-none flex flex-col justify-end items-center flex-1">
+            {navLinks.map((nav: any, index: number) => (
+              <li key={nav.id} className={`font-poppins font-normal cursor-pointer text[16px] ${index === navLinks.length - 1 ? "mr-0" : "mb-4"} text-white`}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
           </ul>
 
-          <div className="sm:hidden flex flex-1 justify-end items-center">
-              <img
-                  src={toggle ? close : menu}
-                  alt="menu"
-                  className="h-[28px] w-[28px] object-contain cursor-pointer"
-                  onClick={() => settoggle((prev) => !prev)}
-              />
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-              <div
-                  className={`${
-                      toggle ? "flex" : "hidden"
-                  } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
-              >
-                  
-                  <ul className="list-none flex flex-col justify-end items-center flex-1">
-                      {navLinks.map((nav: any, index: number) => (
-                          <li
-                              key={nav.id}
-                              className={`font-poppins font-normal cursor-pointer text[16px] ${
-                                  index === navLinks.length - 1
-                                      ? "mr-0"
-                                      : "mb-4"
-                              } text-white`}
-                          >
-                              <a href={`#${nav.id}`}>{nav.title}</a>
-                          </li>
-                      ))}
-                  </ul>
-
-              </div>
-          </div>
-      </nav>
-  )
-}
-
-export default Navbar
+export default Navbar;
