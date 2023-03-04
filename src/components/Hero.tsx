b1809b162cb4d6b5b734b0a5bed1be163dd335d7
Vítor Arimatéa <vitorodev@outlook.com>
2023-03-03 22:32:16 -0300

    Melhorando legibilidade do código

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,21 +2,11 @@ import styles from '../style';
 import {discount, robot} from '../assets';
 import GetStarted from './GetStarted';
 
-
 const Hero = () => (
-    <section
-        id="home"
-        className={`flex md:flex-row flex-col ${styles.paddingY}`}
-    >
-        <div
-            className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
-        >
+    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+        <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
             <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
-                <img
-                    src={discount}
-                    alt="discount"
-                    className="h-[32px] w-[32px]"
-                />
+                <img src={discount} alt="discount" className="h-[32px] w-[32px]" />
                 <p className={`${styles.paragraph} ml-2 uppercase`}>
                     <span className="text-white">20%</span> discount for{" "}
                     <span className="text-white">1 month</span> account
@@ -46,11 +36,7 @@ const Hero = () => (
         </div>
 
         <div className={`flex-1 flex ${styles.flexCenter}`}>
-            <img
-                src={robot}
-                alt="billing"
-                className="h-[100%] w-[100%] relative z-[5]"
-            />
+            <img src={robot} alt="billing" className="h-[100%] w-[100%] relative z-[5]"/>
 
             <div className="absolute z-[0] w-[20%] h-[35%] top-0 pink__gradient" />
             <div className="absolute z-[1] w-[15%] h-[80%] rounded-full bottom-40 white__gradient" />
@@ -60,7 +46,7 @@ const Hero = () => (
             <GetStarted />
         </div>
     </section>
-)
+);
 
 
-export default Hero
+export default Hero;
