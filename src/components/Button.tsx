9bb0c352898ac037a3f63768e8d4fad395a414ea
Vítor Arimatéa <vitorodev@outlook.com>
2023-03-06 12:12:17 -0300

    Alterando de bg blue para green

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Button = ({styles}:any) => (
-    <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] rounded-[10px] text-primary outline-none ${styles}`}>
+    <button type='button' className={`py-4 px-6 bg-green-gradient font-poppins font-medium text-[18px] rounded-[10px] text-primary outline-none ${styles}`}>
       Get Started
     </button>
   );
